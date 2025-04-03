4d8ef27a2d176b2676a245fd47f58d3d6881c414
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2025-04-03 18:32:57 +0200

    Work completly done

---
+++ EntradaDeDatosPorConsola/ProcesadoDeDatos.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaDeDatosPorConsola
+{
+    internal class ProcesadoDeDatos
+    {
+        public int CalcularEdadDentroDeCincoAños(int edad)
+        {
+            return edad + 5;
+        }
+
+        public int CalcularDobleAltura(int altura)
+        {
+            return altura*2;
+        }
+
+        public void MostrarDatos(string nombre, int edad, int altura, bool estudiante)
+        {
+            Console.WriteLine("Nombre: " + nombre + "\n" +
+                              "Edad: " + edad + "\n" +
+                              "Altura: " + altura + "\n" + 
+                              "¿Es estudiante?: " + estudiante);    
+        }
+
+        public string ValidarYPedirDatosNombre(string mensaje)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensaje);
+                string nombre = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(nombre) && nombre.All(c => char.IsLetter(c) || c == ' '))
+                {
+                    return nombre;
+                }
+                Console.WriteLine("Solo se permiten letras, no puedes escribir simbolos ni numeros");
+            }
+        }
+
+
+        public int ValidarYPedirDatosEdad(string mensaje)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensaje);
+                string entrada = Console.ReadLine();
+
+                if (int.TryParse(entrada, out int edad))
+                {
+                    if (edad < 7)
+                    {
+                        Console.WriteLine("Vamos a ver, no puedes tener menos de 7 años porque no sabrías leer");
+                    }
+                    else if (edad > 90)
+                    {
+                        Console.WriteLine("Si tienes más de 90, disfruta la vida en lugar de estudiar... ya te lo ganaste");
+                    }
+                    else
+                    {
+                        return edad;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Solo puedes escribir números enteros. Intenta nuevamente.");
+                }
+            }
+        }
+
+        public int ValidarYPedirDatosAltura(string mensaje)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensaje);
+                string entrada = Console.ReadLine();
+
+                if (int.TryParse(entrada, out int altura))
+                {
+                    if (altura < 65)
+                    {
+                        Console.WriteLine("El record mundial de la persona mas bajita es de 65 cm, no creo que lo puedas superar");
+                    }
+                    else if (altura > 300)
+                    {
+                        Console.WriteLine("O me estas tomando de pelo o serás una super estrella NBA");
+                    }
+                    else
+                    {
+                        return altura;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Solo puedes escribir números enteros. Intenta nuevamente");
+                }
+            }
+        }
+
+        public bool ValidarYPedirDatosEstudiante(string mensaje)
+        {
+            while (true)
+            {
+                Console.WriteLine(mensaje);
+                string input = Console.ReadLine();
+                string respuesta = input.ToUpper();
+                if (respuesta == "SI")
+                {
+                    return true;
+                }
+                else if (respuesta == "NO")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Solo puedes decir \"Si\" o \"No\"");
+                }
+            }
+        }
+
+    }
+}
---
+++ EntradaDeDatosPorConsola/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EntradaDeDatosPorConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola que tal!!");
+            ProcesadoDeDatos procesado = new ProcesadoDeDatos();
+            string nombre = procesado.ValidarYPedirDatosNombre("Me podrías decir tu nombre por favor???:");
+            int edad = procesado.ValidarYPedirDatosEdad("Ahora dime tu edad por favor:");
+            int altura = procesado.ValidarYPedirDatosAltura("Cual es tu altura en centimetros??:");
+            bool estudiante = procesado.ValidarYPedirDatosEstudiante("Por ultimo dime si eres un estudiante: \"Si\" para si o \"No\" para no");
+            Console.WriteLine("la edad que tendrás en 5 años es: " + procesado.CalcularEdadDentroDeCincoAños(edad));
+            Console.WriteLine("El doble de tu altura es: " + procesado.CalcularDobleAltura(altura));
+            procesado.MostrarDatos(nombre, edad, altura, estudiante);
+
+            Console.ReadLine();
+        }
+    }
+}
